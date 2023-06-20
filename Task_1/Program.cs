7d052abd6621387cb842189e519b96947b4e0bbf
Iren <Iren010484@mail.ru>
2023-06-20 10:20:25 +0500

    Добавили логическую функцию в решение 1 задачи

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -3,11 +3,26 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
-Console.WriteLine("Введите трехзначное число");
-string digit = Console.ReadLine(); 
-int three_digit = Convert.ToInt32(digit);
-if (three_digit < 100 || three_digit >=1000) 
-    {Console.WriteLine("Вы ввели не трехзначное число");}
-else
-{int number = three_digit % 100 / 10;
-Console.WriteLine($" Вторая цифра = {number}");} 
+int ReadInt(string message)
+{
+    System.Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine()); 
+    return number;
+}
+
+bool Validate3sing(int numb)
+{
+    if (numb < 100 || numb >=1000) 
+        {   
+            Console.WriteLine("Вы ввели не трехзначное число");
+            return false;
+        }
+    return true;
+}
+
+int three_digit = ReadInt("Введите трехзначное число");
+if (Validate3sing(three_digit))
+{
+    int number = three_digit % 100 / 10;
+    Console.WriteLine($" Вторая цифра = {number}");
+} 
